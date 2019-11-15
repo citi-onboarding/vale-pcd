a7498a8cdaa44841a2e7a0dd935e60753fbb72ee
Daniel Henrique <dhac@cin.ufpe.br>
2019-11-15 10:39:38 -0300

    Estrutura básica do projeto

---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views import generic
+#from .models import "YourModel"
+# Create your views here.
+
+
+class HomeView(generic.TemplateView):
+    template_name = 'home.html'
+
+    """ def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["pessoas"] = People.objects.all() 
+     return context """
