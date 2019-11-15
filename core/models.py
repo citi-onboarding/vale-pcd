36e823857e7670efb4ccbeb3f826c4d33dce2cf1
Daniel Henrique <dhac@cin.ufpe.br>
2019-11-15 15:43:25 -0300

    mudança de ordem dos campos da entidade 'Quem Somos'

--- core/models.py
+++ core/models.py
@@ -5,8 +5,8 @@ from solo.models import SingletonModel
 
 #Seção: Quem Somos
 class QuemSomos(SingletonModel):
-    subtitulo = models.CharField('Frase inicial',max_length=100,help_text="Frase inicial que ficará em destaque")
     descricao = models.TextField('Descrição', max_length=400,help_text="Descrição da seção: 'Quem somos' ")
+    subtitulo = models.CharField('Frase inicial',max_length=100,help_text="Frase inicial que ficará em destaque")
     imagem = models.ImageField(upload_to='quemSomos/', verbose_name='Imagem',help_text="Imagem que ficará na seção: 'Quem Somos' ")
 
 
