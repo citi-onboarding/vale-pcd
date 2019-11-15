e4e88d9cdb0d70c0baaf51fbd8d4d2c285430dbe
Daniel Henrique <dhac@cin.ufpe.br>
2019-11-15 16:41:06 -0300

    Deploy no Heroku

--- valePcd/settings.py
+++ valePcd/settings.py
@@ -127,3 +127,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL =  '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
