b9bd569d6cf7c30af6f16de732ceda875ad19cdd
rrs6 <rrs6@cin.ufpe.br>
2019-11-15 15:49:29 -0300

    Resolve o problema das nomenclaturas

--- core/models.py
+++ core/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Relatos(models.Model):
-    name = models.CharField('Nome', max_length=65)
-    short_text = models.CharField('Texto curto', max_length = 100)
-    relato = models.TextField('Relato',max_length=400)
-    image = models.ImageField(upload_to='relatos/', verbose_name='Imagem', null=True, blank=True)
+    name = models.CharField('Nome', max_length=65, help_text="Insira o seu nome aqui.")
+    description = models.CharField('Texto curto', max_length = 100, help_text="Insira uma breve descrição sobre você.")
+    story = models.TextField('Relato',max_length=400, help_text="Insira seu relato aqui.")
+    image = models.ImageField(upload_to='relatos/', verbose_name='Imagem', null=True, blank=True, help_text="Insira uma imagem sobre a pessoa.")
     class Meta:
         ordering = ['name']
         verbose_name = 'Relato'
--- core/views.py
+++ core/views.py
@@ -10,5 +10,5 @@ class HomeView(generic.TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["relatos"] = Relatos.objects.all() 
+        context["relatos"] = Relatos.objects.all()
         return context
