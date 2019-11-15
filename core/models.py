9e455e09b35ef86cf0f0138b7050d18611a3329e
Daniel Henrique <dhac@cin.ufpe.br>
2019-11-15 15:24:52 -0300

    Correção verbose_name + adicionado help_text  + todos são campos agora são  obrigatórios

--- core/models.py
+++ core/models.py
@@ -5,14 +5,14 @@ from solo.models import SingletonModel
 
 #Seção: Quem Somos
 class QuemSomos(SingletonModel):
-    subtitulo = models.CharField('Subtítulo',max_length=100,blank=True)
-    descricao = models.TextField('Descrição', max_length=400, blank=True)
-    imagem = models.ImageField(upload_to='quemSomos/', verbose_name='Imagem', null=True, blank = True)
+    subtitulo = models.CharField('Frase inicial',max_length=100,help_text="Frase inicial que ficará em destaque")
+    descricao = models.TextField('Descrição', max_length=400,help_text="Descrição da seção: 'Quem somos' ")
+    imagem = models.ImageField(upload_to='quemSomos/', verbose_name='Imagem',help_text="Imagem que ficará na seção: 'Quem Somos' ")
 
 
     #Usado para ajustar o plural
     class Meta:
-        verbose_name = 'QuemSomos'
+        verbose_name = 'Quem Somos'
     #Retorna o nome
     def __str__(self):
         return "Quem Somos"
