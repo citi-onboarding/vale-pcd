d2a3c4d7b17641a9287fa3816d22c113658bec82
Daniel Henrique <dhac@cin.ufpe.br>
2019-11-18 10:43:24 -0300

    ajustar help_text

--- core/models.py
+++ core/models.py
@@ -14,11 +14,11 @@ class Relatos(models.Model):
     def __str__(self):
         return self.name
 
-      
+
 class QuemSomos(SingletonModel):
-    descricao = models.TextField('Descrição', max_length=400,help_text="Descrição da seção: 'Quem somos' ")
+    descricao = models.TextField('Descrição', max_length=400 ")
     subtitulo = models.CharField('Frase inicial',max_length=100,help_text="Frase inicial que ficará em destaque")
-    imagem = models.ImageField(upload_to='quemSomos/', verbose_name='Imagem',help_text="Imagem que ficará na seção: 'Quem Somos' ")
+    imagem = models.ImageField(upload_to='quemSomos/', verbose_name='Imagem')
 
 
     #Usado para ajustar o plural
