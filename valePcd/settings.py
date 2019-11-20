6bd9572c7d539aa2f5112a21b4a80457ec3b8101
Daniel Henrique <dhac@cin.ufpe.br>
2019-11-20 18:07:00 -0300

    Encapsular Senha e email de contato

--- valePcd/settings.py
+++ valePcd/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -139,6 +140,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'valepcd.site@gmail.com'
-EMAIL_HOST_PASSWORD = 'valepcdpta'
+EMAIL_HOST_USER = os.environ.get("email_contato")
+EMAIL_HOST_PASSWORD = os.environ.get("senha_email")
 EMAIL_PORT = 587
