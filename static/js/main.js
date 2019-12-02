01a5866d86a2b1f84dad050982159d3be6a56a2d
Daniel Henrique <dhac@cin.ufpe.br>
2019-12-01 22:08:01 -0300

    Ajustar responsividade

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-$('#contact-form').submit(function(e) {
+$('#contact-form').submit(function (e) {
 
 
     document.getElementById("enviar").innerHTML = 'Enviando Mensagem';
@@ -22,11 +22,11 @@ $('#contact-form').submit(function(e) {
             'assunto': assunto,
             'mensagem': mensagem
         },
-        success: function(data) {
+        success: function (data) {
             alert('Email Enviado');
             document.getElementById("enviar").innerHTML = 'Enviar';
         },
-        error: function(data) {
+        error: function (data) {
             alert('Email não enviado, tente outra forma de contato');
             document.getElementById("enviar").innerHTML = 'Enviar';
         }
@@ -35,18 +35,18 @@ $('#contact-form').submit(function(e) {
 
 });
 
+var contatoTelaAtual = "verde";
+function dadosContato() {
+    /*  document.getElementById('id1').style.color */
+    let estiloFormulario = document.querySelector('#contact-form').style;
+    let estiloDegradeEsquerdo = document.querySelector('#contato-degrade-esquerda').style;
+    let estiloDegradeDireito = document.querySelector('#contato-degrade-direita').style;
+    let estiloContatoRedesSociais = document.querySelector('#contato-redes-sociais').style;
+    let estiloItemsEsquerda = document.querySelector("#contato-container-itemsesquerda").style;
+    let estiloContatoContainer = document.querySelector(".contato-container").style;
+    let estiloBotao = document.querySelector(".container-items-seta").style;
 
-function dadosContato(){
-   /*  document.getElementById('id1').style.color */
-   let estiloFormulario = document.querySelector('#contact-form').style;
-   let estiloDegradeEsquerdo =  document.querySelector('#contato-degrade-esquerda').style;
-   let estiloDegradeDireito = document.querySelector('#contato-degrade-direita').style;
-   let estiloContatoRedesSociais =  document.querySelector('#contato-redes-sociais').style;
-   let estiloItemsEsquerda = document.querySelector("#contato-container-itemsesquerda").style;
-   let estiloContatoContainer = document.querySelector(".contato-container").style;
-   let estiloBotao = document.querySelector(".container-items-seta").style;
-
-    if(estiloFormulario.display == "none"){ //formulário escondido
+    if (estiloFormulario.display == "none") { //formulário escondido
 
         //mostrar formulário
         estiloFormulario.display = "flex";
@@ -63,17 +63,19 @@ function dadosContato(){
         //Ajustar posição do botão
         estiloBotao.position = "";
         estiloBotao.left = "";
-        estiloBotao.transform =  "rotate(0deg)";
+        estiloBotao.transform = "rotate(0deg)";
+
+        contatoTelaAtual = "verde";
 
 
-    }else{ // formulário à mostra
+    } else { // formulário à mostra
 
         //esconder formulário
         estiloFormulario.display = "none";
 
         estiloContatoContainer.paddingTop = "50px";
         estiloItemsEsquerda.width = "100vw";
-        estiloItemsEsquerda.height = "76vh"; 
+        estiloItemsEsquerda.height = "76vh";
         estiloItemsEsquerda.display = "flex";
         estiloDegradeDireito.display = "none";
         estiloDegradeEsquerdo.display = "none";
@@ -81,9 +83,49 @@ function dadosContato(){
 
         //Ajustar posição do botão
         estiloBotao.position = "absolute";
-        estiloBotao.transform =  "rotate(180deg)";
+        estiloBotao.transform = "rotate(180deg)";
         estiloBotao.left = "15vw";
+
+        contatoTelaAtual = "roxo";
 
     }
 
 }
+
+
+window.addEventListener('resize', function () {
+    let windowWidth = window.innerWidth;
+    console.log("width da tela:" + windowWidth);
+
+    if (windowWidth > 800) {
+        //Ajustar responsividade web
+        //ajustar itens da esquerda
+        document.querySelector('#contato-degrade-esquerda').style.display = "flex";
+        document.querySelector("#contato-container-itemsesquerda").style.display = "flex";
+
+        document.querySelector('#contato-degrade-direita').style.display = "flex";
+        document.querySelector('#contato-redes-sociais').style.display = "flex";
+        document.querySelector('#contact-form').style.display = "flex";
+        document.querySelector('#contact-form').style.marginRight = "40%";
+        contatoTelaAtual = "verde";
+
+
+    } else { //ajustar responsividade mobile
+        //escondendo itens da esquerda
+        document.querySelector('#contato-degrade-esquerda').style.display = "none";
+
+        if (contatoTelaAtual == "roxo") {
+
+
+        } else {
+            document.querySelector("#contato-container-itemsesquerda").style.display = "none";
+
+        }
+
+        document.querySelector('#contact-form').style.marginRight = "11%";
+        document.querySelector("#contato-container-itemsesquerda").style.width = "";
+        document.querySelector("#contato-container-itemsesquerda").style.height = "";
+
+
+    }
+});
